eda9221c0afdd3f2c87952d99af11ccae26912a2
Jeffrey Davis <davisj0214@yahoo.com>
2019-09-15 17:05:47 -0500

    intial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Blue Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monte Cristo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
---
+++ models/burger.js
@@ -0,0 +1,17 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function () {
+        orm.selectAll() 
+    },
+
+    insertOne: function () {
+        orm.insertOne()
+    },
+
+    updateOne: function () {
+        orm.updateOne()
+    }
+};
+
+module.exports = burger;
